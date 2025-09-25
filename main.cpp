0d214984f2f12ddddf55739d7b82634ef672ee26
shuxiangfan <linclonfan@outlook.com>
2025-09-25 23:25:56 +0800

    multiple fixes

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@ std::string tarball_name="tarball.tgz";
 std::string decompressed_dir_name="decompressed";
 std::string registryURL;
 
-struct phrased_response {
+struct parsed_response {
     std::string entryfilepath; //index.js path
-    std::string phrased_tarballURL; //tarball URL
+    std::string parsed_tarballURL; //tarball URL
     std::string requested_filepath; // the requested file path(if has)
     //the path starts with "/"
 
@@ -29,7 +29,7 @@ int server();
 
 size_t WriteResponse(char *ptr, size_t size, size_t nmemb, void *userdata);
 
-phrased_response response_phrase(std::string OrigResponse,std::string origurl);
+parsed_response response_parse(std::string OrigResponse,std::string origurl);
 
 std::string fileviewgen(std::string pkgver);
 
@@ -55,9 +55,19 @@ int server() {
     using namespace httplib;
     Server svr;
 
-    svr.Get(".", [&](const Request& req, Response &res) {
+    svr.Get(R"(.*)", [&](const Request& req, Response &res) {
         std::string path = req.target;
+
+        if (!path.empty() && path.front() == '/') {
+            path.erase(0, 1);
+        }
+
+        spdlog::info("The requested URL path={}",path);
+
+        std::regex pattern("@.*");
         std::string finalurl=registryURL+"/"+path;
+        finalurl=std::regex_replace(finalurl,pattern,"");
+        spdlog::info("the request URL={}",finalurl);
 
         curl_global_init(CURL_GLOBAL_ALL);
         CURL* handle=curl_easy_init();
@@ -69,8 +79,13 @@ int server() {
         curl_easy_perform(handle);
         curl_easy_cleanup(handle);
 
-        phrased_response response=response_phrase(OrigResponse,path);
-        std::string tarballURL=response.phrased_tarballURL;
+        parsed_response response=response_parse(OrigResponse,path);
+        std::string tarballURL=response.parsed_tarballURL;
+
+        spdlog::info("The tarballURL={}",response.parsed_tarballURL);
+        spdlog::info("The entryfilepath={}",response.entryfilepath);
+        spdlog::info("The notfound flag={}",response.notfound);
+
 
         if (response.notfound==true) {
             res.status=StatusCode::NotFound_404;
@@ -109,13 +124,25 @@ extern size_t WriteResponse(char *ptr, size_t size, size_t nmemb, void *userdata
     return size * nmemb;
 }
 
-phrased_response response_phrase(std::string OrigResponse,std::string origurl) {
+parsed_response response_parse(std::string OrigResponse,std::string origurl) {
 
-    phrased_response result;
+    parsed_response result;
     std::string pkgver;
+    spdlog::info("Entered phrase url function!\n In phrase: origurl={}",origurl);
+    spdlog::info("In parse: Registry response (first 200 chars): {}", OrigResponse.substr(0, 200));
+
 
     using json=nlohmann::json;
-    json orig_data =json::parse(OrigResponse);
+    json orig_data;
+    try {
+        json orig_data =json::parse(OrigResponse);
+        spdlog::info("In parse: JSON parsed!");
+    }catch (const std::exception &e) {
+        spdlog::error("JSON parse failed: {}", e.what());
+        result.notfound = true;
+        return result;
+    }
+
 
     std::regex findpkgver("@([^/]+)");
     std::regex filepath("^[^@]+@[^/]+(/.*)$");
@@ -124,13 +151,18 @@ phrased_response response_phrase(std::string OrigResponse,std::string origurl) {
 
     if(orig_data.at("error")=="Not found") {
         result.notfound=true;
+        spdlog::info("In phrase: notfound flag=true");
     }
-    else if (std::regex_match(origurl, match, findpkgver)) {
+    else if (std::regex_search(origurl, match, findpkgver)) {
         pkgver=match[1];
+        spdlog::info("In phrase: pkgver={}",pkgver);
+
 
     }
     else {
+        spdlog::info("In phrase: try to find latest");
         pkgver=orig_data["dist-tags"]["latest"];
+        spdlog::info("In phrase: pkgver={}",pkgver);
 
     }
 
@@ -144,6 +176,7 @@ phrased_response response_phrase(std::string OrigResponse,std::string origurl) {
         else {
             //returm the requested file
             result.requested_filepath=requested_file_path;
+            spdlog::info("In phrase: requested file path={}",requested_file_path);
             result.specified_file=true;
         }
     }
@@ -173,7 +206,7 @@ phrased_response response_phrase(std::string OrigResponse,std::string origurl) {
     }
 
     //find the tarball file link
-    result.phrased_tarballURL=pkgjson["dist"].at("tarball");
+    result.parsed_tarballURL=pkgjson["dist"].at("tarball");
 
     return result;
 }
